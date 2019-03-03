12675b8b8c766cf17935977fbe2b725837254e8f
Fabian <fabian.pieringer@student.uni-augsburg.de>
2019-03-03 22:20:53 +0100

    first working docker version running private chain

--- run.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-# Create log directory for Geth
-mkdir -p /var/log
-
-# Start Geth and direct output to stdout
-geth --port 3000 --networkid 42 --nodiscover --datadir=./node0 --maxpeers=0  --rpc --rpcaddr localhost --rpcport 8545 --rpccorsdomain \"*\" --rpcapi \"eth,net,web3,personal,miner\" --mine
